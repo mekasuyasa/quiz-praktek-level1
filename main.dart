d8abf54415dfda14a31ead3a6d7c33aff6b97976
M. Eka Suyasa <157134533+mekasuyasa@users.noreply.github.com>
2024-01-31 17:01:42 +0700

    Menambahkan file main.dart

---
+++ main.dart
@@ -0,0 +1,68 @@
+import 'dart:io';
+import 'hitungzakat.dart';
+import 'luaskotak.dart';
+
+void main() {
+  String pilihan = "";
+  int manggaAmir = 1;
+  int manggaBudi = 1;
+
+  print('Quiz praktikum Level1 oleh M. Eka Suyasa');
+  print('-----------------------------------------');
+  print('KALKULATOR');
+  print('NIP: ARN 161-3539');
+
+  while (pilihan != "q") {
+    print(' ');
+    print('Pilih Menu:');
+    print('1. Penambahan ');
+    print('2. Perkalian ');
+    print('3. Hitung zakat ');
+    print('4. Hitung luas kotak ');
+    print('q. untuk keluar. ');
+    print(' ');
+    print('Pilihan:? ');
+    String pilihan = stdin.readLineSync()!;
+    switch (pilihan) {
+      case '1':
+        print('Dipilih menu penambahan');
+        print('-------------------------');
+        print(
+            'jumlah mangga Amir 10 buah lebih banyak dari jumlah mangga Budi');
+        print('Bila jumlah mangga Budi adalah:?');
+        manggaBudi = int.parse(stdin.readLineSync()!);
+        manggaAmir = 10 + manggaBudi;
+        print('Maka jumlah mangga Amir ada $manggaAmir');
+
+      case '2':
+        print('Dipilih menu perkalian');
+        print('-------------------------');
+        print('jumlah mangga Amir 2 kali lebih banyak dari jumlah mangga Budi');
+        print('Bila jumlah mangga Budi adalah:?');
+        manggaBudi = int.parse(stdin.readLineSync()!);
+        manggaAmir = 2 * manggaBudi;
+        print('Maka jumlah mangga Amir ada $manggaAmir');
+
+      case '3':
+        zakat();
+
+      case '4':
+        luasKotak();
+
+      case 'q':
+        print('Dipilih keluar program');
+
+      default:
+        print('pilihan tidak ada');
+    }
+    if (pilihan == 'q') {
+      break;
+    }
+    print('-------------------------');
+    print('akan menghitung lainnya (y jika ya)?');
+    pilihan = stdin.readLineSync()!;
+    if (pilihan != 'y') {
+      break;
+    }
+  }
+}
